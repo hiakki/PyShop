db9d4cddc52ed190733a358d2257162c909c143b
hiakki <hy.akshay@gmail.com>
2019-10-24 09:36:17 +0530

    final touch

--- pyshop/settings.py
+++ pyshop/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'f16!z$&6*-i$z4$6nzsjvlvio*u9vf0-i5w$f4sx&qlu5yuzcm'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["dev.tricksvibe.tk", "13.127.120.128"]
+ALLOWED_HOSTS = ["dev.tricksvibe.tk", "localhost"]
 
 
 # Application definition
@@ -55,7 +55,9 @@ ROOT_URLCONF = 'pyshop.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates')
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
